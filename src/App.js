5076f5e553011726419b46f0ba3b21676e992f78
SatellitePeace <njokunneoma1999@gmail.com>
2023-09-10 12:53:48 +0100

    made calculations work

--- src/App.js
+++ src/App.js
@@ -1,14 +1,88 @@
+import { useState } from "react";
+import person from "./images/icon-person.svg"
+import dollar from "./images/icon-dollar.svg"
+
+
 function App() {
+  const [bill, setBill] = useState("");
+  const [custom, setCustom] = useState("");
+  const [numOfPeople, setNumOfPeople] = useState("");
+  const [tipPercentage, setTipPercentage] = useState(0); // Store selected tip percentage
+  const [tipAmountPerPerson, setTipAmountPerPerson] = useState(0); // Store calculated tip amount
+  const [totalAmountPerPerson, setTotalAmountPerPerson] = useState(0); // Store calculated total amount
+
+  function handleSubmit(e) {
+    e.preventDefault();
+
+    if (!bill) {
+      return
+    }
+
+    if (!numOfPeople || numOfPeople <= 0) {
+      return
+    }
+
+    const billAmount = parseFloat(bill);
+    const numOfPersons = parseFloat(numOfPeople);
+
+    // Calculate tip amount and total amount
+    const selectedTipPercentage = custom ? parseFloat(custom) : tipPercentage;
+    const tipAmount = (billAmount * selectedTipPercentage) / 100;
+    const totalAmount = billAmount + tipAmount;
+
+    // Calculate tip amount per person and total amount per person
+    const tipAmountPerPerson = tipAmount / numOfPersons;
+    const totalAmountPerPerson = totalAmount / numOfPersons;
+
+    // Set the calculated values in state
+    setTipAmountPerPerson(tipAmountPerPerson);
+    setTotalAmountPerPerson(totalAmountPerPerson);
+
+  }
+
+  function handleTipPercent(e) {
+    const selectedTipPercentage = parseFloat(e.target.getAttribute("value"));
+    setTipPercentage(selectedTipPercentage);
+  }
+
+  function handleReset() {
+    setBill("")
+    setNumOfPeople("")
+
+    setTipAmountPerPerson(0)
+    setTotalAmountPerPerson(0)
+
+    setCustom("")
+    setTipPercentage(0)
+  }
+
   return (
-    <main className="App">
+    <main className="App ">
       <Header />
+
       <section className="app-section">
-        <TipCalculator />
-        <Output />
+        <TipCalculator
+          bill={bill}
+          onAddBill={setBill}
+          custom={custom}
+          onAddCustom={setCustom}
+          numOfPeople={numOfPeople}
+          onAddPeople={setNumOfPeople}
+          onAddSubmit={handleSubmit}
+          onAddPercentage={handleTipPercent}
+          selectedTipPercentage={tipPercentage} // Pass the selected tip percentage
+        />
+
+        <Output
+          tipAmountPerPerson={tipAmountPerPerson}
+          totalAmountPerPerson={totalAmountPerPerson}
+          onReset={handleReset}
+        />
       </section>
     </main>
   );
 }
+
 function Header() {
   return (
     <h1>
@@ -18,43 +92,96 @@ function Header() {
   );
 }
 
-function TipCalculator() {
+function TipCalculator({
+  bill,
+  onAddBill,
+  custom,
+  onAddCustom,
+  numOfPeople,
+  onAddPeople,
+  onAddSubmit,
+  onAddPercentage,
+  selectedTipPercentage,
+}) {
   return (
-    <form>
-      {/* bill input */}
+    <form onSubmit={onAddSubmit}>
+      <input type="submit" style={{ display: "none" }} />
       <label>Bill</label>
-      <input type="text" className="grey-input" />
+      <div className="dollar">
+        <input
+          type="number"
+          className="grey-input"
+          value={bill}
+          placeholder="0"
+          onChange={(e) => onAddBill(e.target.value)}
+        />
+        <img src={dollar} alt="dollar" className="input" />
+      </div>
       <label>Select Tip %</label>
       <div className="tips-container">
-        <button value={5} className="tip-input">
+        <p
+          value={5}
+          className={`tip-input ${selectedTipPercentage === 5 ? "selected" : ""}`}
+          onClick={onAddPercentage}
+        >
           5%
-        </button>
-        <button value={10} className="tip-input">
+        </p>
+        <p
+          value={10}
+          className={`tip-input ${selectedTipPercentage === 10 ? "selected" : ""}`}
+          onClick={onAddPercentage}
+        >
           10%
-        </button>
-        <button value={15} className="tip-input">
+        </p>
+        <p
+          value={15}
+          className={`tip-input ${selectedTipPercentage === 15 ? "selected" : ""}`}
+          onClick={onAddPercentage}
+        >
           15%
-        </button>
-        <button value={25} className="tip-input">
+        </p>
+        <p
+          value={25}
+          className={`tip-input ${selectedTipPercentage === 25 ? "selected" : ""}`}
+          onClick={onAddPercentage}
+        >
           25%
-        </button>
-        <button value={50} className="tip-input">
+        </p>
+        <p
+          value={50}
+          className={`tip-input ${selectedTipPercentage === 50 ? "selected" : ""}`}
+          onClick={onAddPercentage}
+        >
           50%
-        </button>
-        {/* <input type="text" value={10 + "%"} className="tip-input" />
-        <input type="text" value={15 + "%"} className="tip-input" />
-        <input type="text" value={25 + "%"} className="tip-input" />
-        <input type="text" value={50 + "%"} className="tip-input" /> */}
-        <input type="text" placeholder="Custom" className="tip-input-custom" />
+        </p>
+        <input
+          type="number"
+          placeholder="Custom"
+          className="tip-input-custom"
+          value={custom}
+          onChange={(e) => onAddCustom(e.target.value)}
+        />
       </div>
-      {/* Number of people paying */}
-      <label>Number of People</label>
-      <input type="text" className="grey-input" />
+      <div className="count">
+        <label>Number of People</label>
+        <label className={`error-msg ${numOfPeople === "0" ? "" : "error"}`}>Can't be Zero</label>
+      </div>
+      <div className="person">
+        <input
+          type="number"
+          placeholder="0"
+          className={`grey-input ${numOfPeople === "0" ? "show" : ""}`}
+          value={numOfPeople}
+          onChange={(e) => onAddPeople(e.target.value)}
+        />
+        <img src={person} alt="person" className="input" />
+      </div>
     </form>
   );
 }
 
-function Output() {
+function Output({ tipAmountPerPerson, totalAmountPerPerson, onReset }) {
+
   return (
     <div className="output-container">
       <article className="tip-amount">
@@ -62,7 +189,7 @@ function Output() {
           Tip Amount <br />
           <span>/ person</span>
         </p>
-        <h3>$0.00</h3>
+        <h3>${tipAmountPerPerson.toFixed(2)}</h3>
       </article>
 
       <article className="total">
@@ -70,13 +197,14 @@ function Output() {
           Total <br />
           <span>/ person</span>
         </p>
-        <h3>$0.00</h3>
+        <h3>${totalAmountPerPerson.toFixed(2)}</h3>
       </article>
 
-      <button className="reset" disabled>
+      <button className="reset" onClick={onReset} disabled={totalAmountPerPerson === 0 && tipAmountPerPerson === 0} >
         RESET
       </button>
     </div>
   );
 }
+
 export default App;
